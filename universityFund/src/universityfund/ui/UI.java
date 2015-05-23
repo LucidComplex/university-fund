2a45bacc5568ff65672709d172d531185badc8c5
Eli Edrian Tan <eliedrian1996@gmail.com>
2015-05-23 23:45:37 +0800

    implement select donor window

---
+++ universityFund/src/universityfund/ui/UI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universityfund.ui;
+
+/**
+ *
+ * @author tan
+ */
+public interface UI {
+    public void receiveIntent(Object intent);
+}
---
+++ universityFund/src/universityfund/ui/tablemodels/SelectDonorTableModel.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universityfund.ui.tablemodels;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.swing.table.AbstractTableModel;
+import universityfund.db.DbHelper;
+import universityfund.db.models.Donor;
+
+/**
+ *
+ * @author tan
+ */
+public class SelectDonorTableModel extends AbstractTableModel {
+    private List<Donor> donorList;
+    private int cachedIndex;
+    private Donor cachedDonor;
+    
+    public SelectDonorTableModel() {
+        EntityManager em = DbHelper.getEntityManager();
+        donorList =em.createQuery(
+                "SELECT d FROM Donor d", Donor.class
+        ).getResultList();
+        cachedIndex = -1;
+        cachedDonor = null;
+    }
+
+    @Override
+    public int getRowCount() {
+        return donorList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return Donor.COLUMN_NAMES.length;
+    }
+    
+    private Donor cacheDonor(int index) {
+        if (cachedIndex != index) {
+            cachedIndex = index;
+            cachedDonor = donorList.get(index);
+        }
+        return cachedDonor;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return cacheDonor(rowIndex).getValue(columnIndex);
+    }
+
+    public long getDonorIdByRowIndex(int selectedRow) {
+        return donorList.get(selectedRow).getId();
+    }
+    
+}
