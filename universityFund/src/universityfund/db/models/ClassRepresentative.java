593cdf014376e2bd78a07d9fc3c99b3515c9aa22
Eli Edrian Tan <eliedrian1996@gmail.com>
2015-05-23 15:16:47 +0800

    add class representative table

---
+++ universityFund/src/universityfund/db/models/ClassRepresentative.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universityfund.db.models;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author tan
+ */
+
+@Entity
+public class ClassRepresentative implements Serializable {
+    @Id
+    private int year;
+    private Donor representative;
+
+    /**
+     * @return the year
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * @return the representative
+     */
+    public Donor getRepresentative() {
+        return representative;
+    }
+
+    /**
+     * @param representative the representative to set
+     */
+    public void setRepresentative(Donor representative) {
+        this.representative = representative;
+    }
+    
+    
+}
